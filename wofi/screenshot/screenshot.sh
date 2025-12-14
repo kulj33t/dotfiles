63aea1124c833b468d7e3c4cb5f91d63a79a8826
Kuljeet Singh <kulj33tsingh@gmail.com>
2025-12-14 20:20:07 +0530

    added screenshot tool using wofi (grim+slurp)

---
+++ wofi/screenshot/screenshot.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SCREENSHOT_DIR="$HOME/screenshots"
+TIMESTAMP="$(date +'%Y-%m-%d-%H-%M-%S')"
+FILENAME="screenshot-${TIMESTAMP}.png"
+
+if [[ ! -d "$SCREENSHOT_DIR" ]]; then
+    mkdir -p "$SCREENSHOT_DIR"
+fi
+
+OPT_FULL="Full Screen"
+OPT_SELECT="Select Area"
+
+CHOICE=$(echo -e "${OPT_FULL}\n${OPT_SELECT}" | wofi --show dmenu --prompt "Screenshot" --width 250 --height 160 --location 0 --style "$HOME/.config/wofi/screenshot/style.css" --cache-file /dev/null)
+
+case "$CHOICE" in
+    "$OPT_FULL")
+        sleep 0.5
+        grim "${SCREENSHOT_DIR}/${FILENAME}"
+        ;;
+    "$OPT_SELECT")
+        grim -g "$(slurp)" "${SCREENSHOT_DIR}/${FILENAME}"
+        ;;
+esac
