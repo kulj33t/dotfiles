a81af54161fcb7a5b34b831017703cf0d8e998b1
Kuljeet Singh <kulj33tsingh@gmail.com>
2025-12-17 02:13:09 +0530

    UI updates and Added Shortcuts

--- waybar/scripts/kbdbacklight.sh
+++ waybar/scripts/kbdbacklight.sh
@@ -3,23 +3,32 @@
 # Device Name
 DEVICE="kbd_backlight"
 
+# Get current percentage directly
 PERCENT=$(brightnessctl -d $DEVICE -m | cut -d, -f4 | tr -d %)
 
-# TOGGLE MODE
+# --- TOGGLE MODE ---
 if [ "$1" == "toggle" ]; then
     if [ "$PERCENT" -le 5 ]; then
+        # 0% -> 10%
+        brightnessctl -d $DEVICE set 10%
+    elif [ "$PERCENT" -le 15 ]; then
+        # 10% -> 50%
         brightnessctl -d $DEVICE set 50%
     elif [ "$PERCENT" -le 60 ]; then
+        # 50% -> 100%
         brightnessctl -d $DEVICE set 100%
     else
+        # 100% -> 0%
         brightnessctl -d $DEVICE set 0%
     fi
     exit
 fi
 
-# STATUS MODE
+# --- STATUS MODE ---
 if [ "$PERCENT" -le 5 ]; then
     echo '{"text": "", "class": "zero", "tooltip": "Keyboard: Off"}'
+elif [ "$PERCENT" -le 15 ]; then
+    echo '{"text": "", "class": "low", "tooltip": "Keyboard: 10%"}'
 elif [ "$PERCENT" -le 60 ]; then
     echo '{"text": "", "class": "half", "tooltip": "Keyboard: 50%"}'
 else
